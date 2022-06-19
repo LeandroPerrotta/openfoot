db2d59da58b4c859307dc79b2aaac9b3c7e1e470
Leandro <leandro_perrotta@hotmail.com>
2022-06-19 15:52:54 -0300

    feature(game): added simple ball animation

--- front/src/views/game/game.ts
+++ front/src/views/game/game.ts
@@ -9,38 +9,74 @@ export class Game {
 
     private timer: number = 0;
     private score: GameScore = { home: 0, visitor: 0 };
+    private canvas: HTMLCanvasElement | null = null;
+    private context: CanvasRenderingContext2D | null = null;
+    private ball: CanvasImageSource;
 
     constructor() {
 
+        this.ball = new Image();
+        this.ball.src = 'assets/game/ball.png'
+
         Connection.registerMessageCallback(ServerMessageType.GameTimer, this.onGameTimer.bind(this));
+    }
+
+    oncreate() {
+
+        this.canvas = document.getElementById('game-balance-canvas') as HTMLCanvasElement;
+
+        this.canvas.width = document.getElementsByClassName('game-balance')[0].clientWidth;
+        this.canvas.height = document.getElementsByClassName('game-balance')[0].clientHeight;
+
+        this.context = this.canvas.getContext('2d');
     }
 
     view() {
         return m('.game', [
 
-            m('.container', [
-                m('.game-bar.row', [
-                    m('.team-home.col-sm-4.row', [
-                        m('.team-home-name.col-sm-10', 'Time da casa'),
-                        m('.team-home-score.col-sm-2.d-flex.justify-content-center', this.score.home)
-                    ]),
-
-                    m('.center.col-sm-4.d-flex.justify-content-center', [
-                        m('.timer', this.timer)
-                    ]),
-
-                    m('.team-visitor.col-sm-4.row', [
-                        m('.team-visitor-score.col-sm-2.d-flex.justify-content-center', this.score.visitor),
-                        m('.team-visitor-name.col-sm-10', 'Time visitante')   
-                    ]),
+            m('.game-bar', [
+                m('.team-home', [
+                    m('.team-home-name', 'Time da casa'),
+                    m('.team-home-score', this.score.home)
                 ]),
 
-                m('.game-bar-controls', [
-                    m('button.btn.btn-warning', { type: 'button', onclick: this.onGameStart.bind(this) }, 'Play!')
-                ])
-                
+                m('.center', [
+                    m('.timer', this.timer)
+                ]),
+
+                m('.team-visitor', [
+                    m('.team-visitor-score', this.score.visitor),
+                    m('.team-visitor-name', 'Time visitante')
+                ]),
+            ]),
+
+            m('.game-balance', [
+                m('canvas#game-balance-canvas')
+            ]),
+
+            m('.game-bar-controls', [
+                m('button.btn.btn-warning', { type: 'button', onclick: this.onGameStart.bind(this) }, 'Play!')
             ])
         ])
+    }
+
+    renderBall(currentBalance: number) {
+
+        if(!this.context || !this.canvas) {
+
+            console.error('No canvas found');
+            return;
+        }
+
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);     
+
+        console.log(this.canvas.width, this.canvas.height)
+
+        const width = this.canvas.width;
+        const y = 1;
+        const x = this.canvas.width / 100 * currentBalance;
+
+        this.context.drawImage(this.ball, x, y);
     }
 
     onGameStart() {
@@ -55,6 +91,7 @@ export class Game {
         this.score = data.score;
 
         m.redraw();
+        this.renderBall(data.balance)
 
         console.log('Timer', this.timer)
     }
