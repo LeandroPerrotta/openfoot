ec94652581cad4f477792153b3c17f0c448fdfb0
Leandro <leandro_perrotta@hotmail.com>
2022-06-25 15:10:01 -0300

    improvement(game): balance changes more randomly

--- game/src/modules/game/game.ts
+++ game/src/modules/game/game.ts
@@ -3,7 +3,7 @@ import { Tools } from "../../shared";
 import { GameScore } from "../../shared/game.type";
 import { ClientMessageTypes, ServerMessageType } from "../../shared/net/net.type";
 
-export class Game{
+export class Game {
 
     private client: Client;
 
@@ -11,78 +11,88 @@ export class Game{
     private score: GameScore = { home: 0, visitor: 0 };
     private balance = 50;
 
-    constructor(client: Client){
+    constructor(client: Client) {
 
         this.client = client;
     }
 
-    startGame(){
+    startGame() {
 
         this.onThink();
     }
 
-    endGame(){
+    endGame() {
 
-        for(const index in Game._games){
+        for (const index in Game._games) {
 
-            if(this === Game._games[index]){
+            if (this === Game._games[index]) {
                 delete Game._games[index];
                 return;
             }
         }
     }
 
-    ballControl(){
+    ballControl() {
 
         const randomValue = Tools.random(1, 100);
-        let balanceChange = randomValue >= 50 ? 5 : -5;
+        let balanceChange;
+
+        if (randomValue <= 33) {
+            balanceChange = -Tools.random(2, 6);
+        }
+        else if (randomValue <= 66) {
+            balanceChange = Tools.random(0, 2);;
+        }
+        else{
+            balanceChange = Tools.random(2, 6);
+        }
 
         /* contra ataque */
-        if(this.balance >= 70 && balanceChange < 0 && Tools.random(1, 100) < 10) {
+        if (this.balance >= 70 && balanceChange < 0 && Tools.random(1, 100) < 10) {
 
             balanceChange = -Tools.random(50, 70);
         }
-        else if(this.balance <= 30 && balanceChange > 0 && Tools.random(1, 100) < 10){
+        else if (this.balance <= 30 && balanceChange > 0 && Tools.random(1, 100) < 10) {
 
             balanceChange = Tools.random(50, 70);
         }
 
         this.balance = Math.max(Math.min(this.balance + balanceChange, 100), 0);
 
-        if(this.balance == 100) {
+        if (this.balance == 100) {
 
             this.score.home++;
             this.balance = 50;
         }
-        else if(this.balance == 0) {
+        else if (this.balance == 0) {
 
             this.score.visitor++;
             this.balance = 50;
         }
     }
 
-    onThink(){
+    onThink() {
 
         this.timer = this.timer + 1;
         this.ballControl();
 
         this.client.send(ServerMessageType.GameTimer, { timer: this.timer, balance: this.balance, score: this.score });
 
-        if(this.timer < 90){
+        if (this.timer < 90) {
             setTimeout(this.onThink.bind(this), 1000)
         }
-        else{
+        else {
             this.endGame();
         }
     }
 
     private static _games: Game[] = [];
-    static init(){
+    static init() {
 
         Client.registerMessageCallback(ClientMessageTypes.GameStart, this.onPlayerStartGame)
     }
 
-    static onPlayerStartGame(client: Client){
+    static onPlayerStartGame(client: Client) {
 
         console.log('Player started game!')
 
