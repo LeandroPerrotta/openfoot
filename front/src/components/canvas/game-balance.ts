dbdbea82375bed87405b7160398321d55b72ba1c
Leandro <leandro_perrotta@hotmail.com>
2022-06-25 15:09:06 -0300

    improvement(game-balance): replaced loop event from JS timeout to animationFrame * it also has improvement in the animations

--- front/src/components/canvas/game-balance.ts
+++ front/src/components/canvas/game-balance.ts
@@ -1,15 +1,21 @@
-const FRAMERATE = 10;
+const FRAMERATE = 20;
 
 export class GameBalance {
 
     private canvas: HTMLCanvasElement;
-    private context: CanvasRenderingContext2D | null;
+    private context: CanvasRenderingContext2D;
     private ball: CanvasImageSource;
     private resetPosition: number;
     private ballPosition: number;
     private ballMovingTo: number;
-    private event: NodeJS.Timeout;
     private calculatedStepSize: number;
+
+    private randomStepDirection: number = 0;
+    private randomStepLastRefresh: number = 0;
+
+    private frameLast: number = 0;
+    private frameRate: number = 1000 / FRAMERATE;
+    private frameTick: number = 0;
 
     constructor() {
 
@@ -20,6 +26,13 @@ export class GameBalance {
 
         this.canvas = document.getElementById('game-balance-canvas') as HTMLCanvasElement;
 
+        const context = this.canvas.getContext('2d');
+        if (!context) {
+            throw new Error('No context found');
+        }
+
+        this.context = context;
+
         const balance = document.getElementsByClassName('game-balance')[0];
 
         this.canvas.width = balance.clientWidth;
@@ -28,57 +41,60 @@ export class GameBalance {
         this.resetPosition = balance.clientWidth / 2 - 10;
         this.ballPosition = this.resetPosition;
         this.ballMovingTo = -1;
-        this.calculatedStepSize = balance.clientWidth / 100 * 1;
-        console.log('calculatedStepSize' , this.calculatedStepSize)
+        this.calculatedStepSize = balance.clientWidth / 100 * 0.5;
+        console.log('calculatedStepSize', this.calculatedStepSize)
 
-        this.context = this.canvas.getContext('2d');      
-        
-        this.event = setTimeout(this.renderBall.bind(this), 1000 / FRAMERATE)
+        this.animate();
+    }
+
+    private animate() {
+
+        this.frameTick = requestAnimationFrame(this.animate.bind(this))
+
+        if (Date.now() - this.frameLast > this.frameRate) {
+
+            this.frameLast = Date.now();
+            this.renderBall()
+        }
     }
 
     private renderBall() {
 
-        if(!this.context || !this.canvas) {
-
-            console.error('No canvas found');
-            return;
-        }
-
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);     
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         const y = 1;
 
-        if(this.ballMovingTo > 0){
-            
+        if (this.ballMovingTo > 0) {
+
             let distance = Math.abs(this.ballPosition - this.ballMovingTo);
             let stepSpeed = this.calculatedStepSize;
 
-            if(distance > stepSpeed / 3){
+            //a bola esta distante o sulficiente para andar na direção ao alvo
+            if (distance > stepSpeed * 1) {
 
-                if(distance >= stepSpeed * 20) stepSpeed = stepSpeed * 5
-                else if(distance <= stepSpeed * 5) stepSpeed = stepSpeed / 3
-                
+                if (distance >= stepSpeed * 20) stepSpeed = stepSpeed * 5
+                else if (distance <= stepSpeed * 5) stepSpeed = stepSpeed / 2
+
                 const stepSize = this.ballMovingTo >= this.ballPosition ? stepSpeed : -stepSpeed;
                 this.ballPosition = Math.max(Math.min(this.ballPosition + stepSize, this.canvas.width), 0);
-            } 
+            }
         }
 
         this.context.drawImage(this.ball, this.ballPosition, y);
-        this.event = setTimeout(this.renderBall.bind(this), 1000 / FRAMERATE)
-    }    
+    }
 
-    moveBallTo(position: number, reset: Boolean = false){
+    moveBallTo(position: number, reset: Boolean = false) {
 
-        if(reset){
+        if (reset) {
 
             this.ballPosition = this.resetPosition;
         }
-            
+
 
         this.ballMovingTo = this.canvas.width / 100 * position;
     }
 
-    stop(){
+    stop() {
 
-        clearTimeout(this.event)
+        cancelAnimationFrame(this.frameTick)
     }
 }   
